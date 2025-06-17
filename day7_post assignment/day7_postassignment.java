f87a367e45c344404282cdf5ed8a8ada2510b17a
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-06-17 09:36:21 +0530

    task completed

---
+++ day7_post assignment/day7_postassignment.java
@@ -0,0 +1,70 @@
+package dsa;
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class day7_postassignment {
+	
+	
+
+	public class PostAssignment {
+
+	    void mergeSort(int[] arr, int left, int right) {
+	        if (left < right) {
+	            int mid = (left + right) / 2;
+
+	            mergeSort(arr, left, mid);
+	            mergeSort(arr, mid + 1, right);
+
+	            merge(arr, left, mid, right);
+	        }
+	    }
+
+	    void merge(int[] arr, int left, int mid, int right) {
+	        int[] temp = new int[right - left + 1];
+
+	        int i = left, j = mid + 1, k = 0;
+
+	        while (i <= mid && j <= right) {
+	            if (arr[i] <= arr[j]) {
+	                temp[k++] = arr[i++];
+	            } else {
+	                temp[k++] = arr[j++];
+	            }
+	        }
+
+	        while (i <= mid) temp[k++] = arr[i++];
+	        while (j <= right) temp[k++] = arr[j++];
+
+	        for (k = 0; k < temp.length; k++) {
+	            arr[left + k] = temp[k];
+	        }
+	    }
+
+	    public static void main(String[] args) {
+	    	Scanner s =new Scanner(System.in);
+			System.out.println("\n Enter the array length:");
+			int size = s.nextInt();
+			int arr[]=new int[size];
+			System.out.println("\n Enter the array elements:");
+			for(int i=0;i<size;i++)
+			{
+				arr[i]=s.nextInt();
+			}
+	        System.out.println(Arrays.toString(arr));
+	        
+
+	        day7_postassignment sorter = new day7_postassignment();
+	        sorter.mergeSort(arr, 0, arr.length - 1);
+
+	        System.out.println(Arrays.toString(arr));
+	    }
+	}
+
+	public void mergeSort(int[] arr, int i, int j) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+	
+
+
